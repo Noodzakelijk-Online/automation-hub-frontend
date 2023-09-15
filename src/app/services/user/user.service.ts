65c1cfb7d92949bd5b450c2c8e748766d420bd3f
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-15 17:31:55 +0200

    feat: update user

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUserModel} from "../../models/user.model.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = '/api/v1/user';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<IUserModel> {
+    return this.http.get<IUserModel>(`${this.apiUrl}/current`);
+  }
+
+    updateUser(user: IUserModel): Observable<IUserModel> {
+        return this.http.patch<IUserModel>(`${this.apiUrl}/current`, user);
+    }
+}
