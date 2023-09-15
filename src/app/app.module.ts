8f1faa71fb653a25fd52af2225812fbea9488c91
Jackson Barreto <jackson.barreto@gmail.com>
2023-09-15 13:27:23 +0200

    feat: change login from local storage to coookie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,26 @@ import {en_US} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {LoginModule} from "./pages/login/login.module";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AuthInterceptorService} from "./interceptors/auth-interceptor.service";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LoginModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
-  providers: [
-    {provide: NZ_I18N, useValue: en_US},
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LoginModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+    ],
+    providers: [
+        {provide: NZ_I18N, useValue: en_US},
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/services/auth.service.interface.ts
+++ src/app/services/auth.service.interface.ts
@@ -2,9 +2,8 @@ import {Observable} from "rxjs";
 import {IUserModel} from "../models/user.model.interface";
 
 export interface IAuthService {
-  login(username: string, password: string): Observable<{token: string}>;
+  login(username: string, password: string): Observable<Object>;
   logout(): Observable<void>;
   loggedIn(): Observable<boolean>;
-  refreshToken(): Observable<{token: string}>;
   getUser(): Observable<IUserModel>;
 }
