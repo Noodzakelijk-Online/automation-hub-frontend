e282dcfd99e136a83cf551a2ddaa849b4bf0cfd2
unknown <rfcosta85@gmail.com>
2023-09-28 19:41:59 -0300

    button removed, form function created, remove button behavior created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {authGuard} from "./services/auth/guards/auth.guard";
-import {RedirectIfLoggedGuard} from "./services/auth/guards/login.guard";
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { authGuard } from './services/auth/guards/auth.guard'
+import { RedirectIfLoggedGuard } from './services/auth/guards/login.guard'
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [RedirectIfLoggedGuard] },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [authGuard] },
-  //{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+    // canActivate: [RedirectIfLoggedGuard],
+  },
+  // { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [authGuard] },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home'},
-];
+  { path: '**', redirectTo: '/home' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/home/modals/automations-form/automations-form.component.ts
+++ src/app/pages/home/modals/automations-form/automations-form.component.ts
@@ -1,123 +1,142 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {NzUploadChangeParam, NzUploadFile} from "ng-zorro-antd/upload";
-import {IAutomationModel} from "../../../../models/automation.model.interface";
-import {NzNotificationService} from "ng-zorro-antd/notification";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { NzUploadChangeParam, NzUploadFile } from 'ng-zorro-antd/upload'
+import { IAutomationModel } from '../../../../models/automation.model.interface'
+import { NzNotificationService } from 'ng-zorro-antd/notification'
 
 @Component({
-    selector: 'app-automations-form-modal',
-    templateUrl: './automations-form.component.html',
-    styleUrls: ['./automations-form.component.scss']
+  selector: 'app-automations-form-modal',
+  templateUrl: './automations-form.component.html',
+  styleUrls: ['./automations-form.component.scss'],
 })
 export class AutomationsFormComponent implements OnInit {
-    modalTitle: string = '';
-    removeImage: boolean = false;
-    selectedImage: File | undefined;
-    @Input() isUpdate: boolean = false;
-    @Input() isVisible: boolean = false;
-    @Input() uploadUrl: string = '';
-    @Output() modalClosed = new EventEmitter<void>();
-    @Output() formDataSubmitted = new EventEmitter<IAutomationModel>();
-    automationForm!: FormGroup;
+  modalTitle: string = ''
+  removeImage: boolean = false
+  selectedImage: File | undefined
+  @Input() isUpdate: boolean = false
+  @Input() isVisible: boolean = false
+  @Input() uploadUrl: string = ''
+  @Output() modalClosed = new EventEmitter<void>()
+  @Output() formDataSubmitted = new EventEmitter<IAutomationModel>()
+  automationForm!: FormGroup
 
-    constructor(private fb: FormBuilder, private notification: NzNotificationService,) {
+  constructor(
+    private fb: FormBuilder,
+    private notification: NzNotificationService
+  ) {}
+
+  ngOnInit() {
+    this.onInitForm()
+  }
+
+  onInitForm() {
+    this.automationForm = this.fb.group({
+      id: [null],
+      name: [
+        '',
+        { validators: [Validators.required, Validators.maxLength(50)] },
+      ],
+      host: [
+        '',
+        { validators: [Validators.required, Validators.maxLength(50)] },
+      ],
+      port: [
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.min(1),
+            Validators.max(65535),
+          ],
+        },
+      ],
+    })
+  }
+
+  beforeUpload = (file: NzUploadFile, fileList: NzUploadFile[]): boolean => {
+    this.selectedImage = file as unknown as File
+    console.log('file in beforeUpload: ', this.selectedImage)
+    return false // Impede o envio automático
+  }
+
+  handleImageUpload({ file, fileList }: NzUploadChangeParam): void {
+    this.selectedImage = file.originFileObj // Armazene o objeto de arquivo original
+    console.log('status: ', file.status)
+    console.log('file: ', file)
+    console.log('file - selectedImage: ', this.selectedImage)
+    const status = file.status
+    if (status !== 'uploading') {
+      console.log(file, fileList)
+    }
+    if (status === 'done') {
+      this.notification.create(
+        'success',
+        'Upload Successful',
+        `${file.name} image uploaded successfully.`
+      )
+      console.log('status done : ', file)
+      //this.automationForm.get('image')?.setValue(file.response.imagePath);
+    } else if (status === 'error') {
+      this.notification.create(
+        'error',
+        'Upload Failed',
+        `${file.name} image upload failed.`
+      )
+    }
+  }
+
+  onSubmit(): void {
+    console.log('imageFile in onSubmit: ', this.selectedImage)
+    if (!this.automationForm.valid) {
+      for (const i in this.automationForm.controls) {
+        this.automationForm.controls[i].markAsDirty()
+        this.automationForm.controls[i].updateValueAndValidity()
+      }
+      return
     }
 
-    ngOnInit() {
-        this.automationForm = this.fb.group({
-            id: [null],
-            name: ['', {validators: [Validators.required, Validators.maxLength(50)]}],
-            host: ['', {validators: [Validators.required, Validators.maxLength(50)]}],
-            port: ['', {validators: [Validators.required, Validators.min(1), Validators.max(65535)]}],
-        });
+    const automation: IAutomationModel = {
+      ...this.automationForm.value,
+      position: 0,
+      imageFile: this.selectedImage,
+      removeImage: this.removeImage,
     }
 
-    beforeUpload = (file: NzUploadFile, fileList: NzUploadFile[]): boolean => {
-        this.selectedImage = file as unknown as File;
-        console.log('file in beforeUpload: ', this.selectedImage);
-        return false; // Impede o envio automático
-    };
-
-
-    handleImageUpload({file, fileList}: NzUploadChangeParam): void {
-        this.selectedImage = file.originFileObj; // Armazene o objeto de arquivo original
-        console.log('status: ', file.status);
-        console.log('file: ', file);
-        console.log('file - selectedImage: ', this.selectedImage);
-        const status = file.status;
-        if (status !== 'uploading') {
-            console.log(file, fileList);
-        }
-        if (status === 'done') {
-            this.notification.create(
-                'success',
-                'Upload Successful',
-                `${file.name} image uploaded successfully.`
-            );
-            console.log('status done : ', file)
-            //this.automationForm.get('image')?.setValue(file.response.imagePath);
-
-        } else if (status === 'error') {
-            this.notification.create(
-                'error',
-                'Upload Failed',
-                `${file.name} image upload failed.`
-            );
-        }
+    if (!automation.id) {
+      delete automation.id
     }
 
-    onSubmit(): void {
-        console.log('imageFile in onSubmit: ', this.selectedImage);
-        if (!this.automationForm.valid) {
-            for (const i in this.automationForm.controls) {
-                this.automationForm.controls[i].markAsDirty();
-                this.automationForm.controls[i].updateValueAndValidity();
-            }
-            return;
-        }
+    this.formDataSubmitted.emit(automation)
+    this.closeModal()
+  }
 
-        const automation: IAutomationModel = {
-            ...this.automationForm.value,
-            position: 0,
-            imageFile: this.selectedImage,
-            removeImage: this.removeImage
-        }
-
-        if (!automation.id) {
-            delete automation.id;
-        }
-
-        this.formDataSubmitted.emit(automation);
-        this.closeModal();
+  openModal(automation?: IAutomationModel, isUpdate: boolean = false) {
+    this.isVisible = true
+    this.isUpdate = isUpdate
+    if (automation) {
+      this.automationForm.patchValue({
+        id: automation.id,
+        name: automation.name,
+        host: automation.host,
+        port: automation.port,
+      })
+      this.automationForm.get('image')?.setValue(automation.image)
     }
-
-    openModal(automation?: IAutomationModel, isUpdate: boolean = false) {
-        this.isVisible = true;
-        this.isUpdate = isUpdate;
-        if (automation) {
-            this.automationForm.patchValue({
-                id: automation.id,
-                name: automation.name,
-                host: automation.host,
-                port: automation.port,
-            });
-            this.automationForm.get('image')?.setValue(automation.image);
-        }
-        if ((!this.modalTitle || this.modalTitle === '') && !this.isUpdate) {
-            this.modalTitle = 'Add Automation';
-        } else if ((!this.modalTitle || this.modalTitle === '') && this.isUpdate) {
-            this.modalTitle = 'Update Automation';
-        }
+    if ((!this.modalTitle || this.modalTitle === '') && !this.isUpdate) {
+      this.modalTitle = 'Add Automation'
+    } else if ((!this.modalTitle || this.modalTitle === '') && this.isUpdate) {
+      this.modalTitle = 'Update Automation'
     }
+  }
 
-    closeModal() {
-        this.isVisible = false;
-        this.modalClosed.emit();
-        this.automationForm.reset();
-        this.modalTitle = '';
-    }
+  closeModal() {
+    this.isVisible = false
+    this.modalClosed.emit()
+    this.automationForm.reset()
+    this.modalTitle = ''
+  }
 
-    deleteImage() {
-        this.removeImage = true;
-    }
+  deleteImage() {
+    this.removeImage = true
+  }
 }
