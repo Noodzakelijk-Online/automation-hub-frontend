4f3c33cd88c0bd7109871e565d872f0aedbcbbdd
Rodrigo Costa <rfcosta85@gmail.com>
2023-10-05 06:05:51 -0300

    change updatedOn atribute from method onInitForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { authGuard } from './services/auth/guards/auth.guard'
-import { RedirectIfLoggedGuard } from './services/auth/guards/login.guard'
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { authGuard } from "./services/auth/guards/auth.guard";
+import { RedirectIfLoggedGuard } from "./services/auth/guards/login.guard";
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: "login",
     loadChildren: () =>
-      import('./pages/login/login.module').then((m) => m.LoginModule),
-    // canActivate: [RedirectIfLoggedGuard],
+      import("./pages/login/login.module").then((m) => m.LoginModule),
+    canActivate: [RedirectIfLoggedGuard],
   },
-  // { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [authGuard] },
   {
-    path: 'home',
+    path: "home",
     loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomeModule),
+      import("./pages/home/home.module").then((m) => m.HomeModule),
+    canActivate: [authGuard],
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home' },
-]
+  // {
+  //   path: 'home',
+  //   loadChildren: () =>
+  //     import('./pages/home/home.module').then((m) => m.HomeModule),
+  // },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", redirectTo: "/home" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,18 @@
-import { Component, Inject, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { NzNotificationService } from 'ng-zorro-antd/notification'
-import { AUTH_SERVICE_TOKEN } from '../../services/auth/auth.service.token'
-import { IAuthService } from '../../services/auth.service.interface'
-import { Router } from '@angular/router'
+import { Component, Inject, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NzNotificationService } from "ng-zorro-antd/notification";
+import { AUTH_SERVICE_TOKEN } from "../../services/auth/auth.service.token";
+import { IAuthService } from "../../services/auth.service.interface";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  hidePassword: boolean = true
-  validateForm: FormGroup = this.fb.group({})
+  hidePassword: boolean = true;
+  validateForm: FormGroup = this.fb.group({});
 
   constructor(
     private fb: FormBuilder,
@@ -22,53 +22,53 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.onInitForm()
+    this.onInitForm();
   }
 
   onInitForm() {
     this.validateForm = this.fb.group({
       userName: [
-        '',
+        "",
         {
-          updateOn: 'blur',
+          updateOn: "blur",
           validators: [Validators.required, Validators.email],
         },
       ],
-      password: ['', { updateOn: 'blur', validators: [Validators.required] }],
+      password: ["", { updateOn: "submit", validators: [Validators.required] }],
       remember: [true],
-    })
+    });
   }
 
   submitForm(): void {
     if (!this.validateForm.valid) {
       for (const i in this.validateForm.controls) {
-        this.validateForm.controls[i].markAsDirty()
-        this.validateForm.controls[i].updateValueAndValidity()
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
       }
-      return
+      return;
     }
 
     this.authService
       .login(this.validateForm.value.userName, this.validateForm.value.password)
       .subscribe({
         next: (response) => {
-          this.router.navigate(['/home']).catch(() => {})
+          this.router.navigate(["/home"]).catch(() => {});
         },
         error: (error) => {
           if (error.status === 401) {
             this.notification.create(
-              'error',
-              'Login Failed',
-              'Incorrect username or password.'
-            )
+              "error",
+              "Login Failed",
+              "Incorrect username or password."
+            );
           } else {
             this.notification.create(
-              'error',
-              'Network Error',
-              'An unexpected error occurred. Please try again later.'
-            )
+              "error",
+              "Network Error",
+              "An unexpected error occurred. Please try again later."
+            );
           }
         },
-      })
+      });
   }
 }
